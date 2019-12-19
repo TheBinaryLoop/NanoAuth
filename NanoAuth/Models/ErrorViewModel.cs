851fcb3f64a041859e5f58848ce3f51c9bfcfbf0
Lukas Eßmann <l.essmann@binarypanther.com>
2019-12-19 08:13:33 +0100

    Initial project structure

---
+++ NanoAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NanoAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
